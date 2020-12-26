61082cc2802685f1f558f6ebf0dafa7a7f37b08a
Alexey <krivonosov2004@mail.ru>
2020-12-26 19:11:42 +0500

    made start map and some fixes.

--- test_physics.py
+++ test_physics.py
@@ -10,26 +10,35 @@ import pytmx
 def main():
     pygame.init()
     pygame.font.init()
-    screen = pygame.display.set_mode((600, 450))
+    size = width, height = (600, 450)
+    screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     moving_right = False
     moving_left = False
     moving_up = False
     running = True
     world = b.b2World()
-    world.gravity = (0, -5)
+    world.gravity = (0, -15)
     ground = world.CreateStaticBody(
         position=(0, -5), shapes=b.b2PolygonShape(box=(59, 5)))
     obj = world.CreateDynamicBody(
         angle=0, position=(10, 22), shapes=b.b2PolygonShape(box=(5, 5)))
+    obj.gravityScale = 0.0  # ставит гравитацию для обЪекта на ноль
+    # player_image = pygame.image.load('project.png')
+    # player_location = [100, height - 220]
+    # pygame.Rect(100, height - 220, player_image.get_width(),
+    #            player_image.get_height())
     while running:
         screen.fill((220, 220, 0))
         if moving_left:
-            obj.ApplyLinearImpulse(b2Vec2(-2, 0), (2.5, 2.5), True)
+            obj.ApplyLinearImpulse(b2Vec2(-0.3, 0), (2.5, 2.5), True)
         if moving_right:
-            obj.ApplyForceToCenter(b2Vec2(50, 0), True)
+            # obj.ApplyForceToCenter(b2Vec2(50, 0), True)
+            obj.ApplyLinearImpulse(b2Vec2(0.3, 0), (2.5, 2.5), True)
         if moving_up:
-            obj.ApplyForceToCenter(b2Vec2(0, 50), True)
+            # obj.ApplyForceToCenter(b2Vec2(0, 50), True)
+            obj
+            obj.ApplyLinearImpulse(b2Vec2(0, 0.3), (2.5, 2.5), True)
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
@@ -48,6 +57,7 @@ def main():
                 if event.key == K_UP:
                     moving_up = False
         drawPolygons(screen, obj)
+        # screen.blit(player_image, player_location)
         # drawPolygons(screen, ground)
         world.Step(1 / 60, 10, 10)
         pygame.display.flip()
@@ -56,10 +66,14 @@ def main():
 
 def drawPolygons(screen, body):
     for fixture in body.fixtures:
+        print(fixture)
         shape = fixture.shape
         vertices = [body.transform * v * 10 for v in shape.vertices]
+        print(vertices)
+        print(vertices)
         vertices = [(v[0], 450 - v[1]) for v in vertices]
-        name = pygame.draw.polygon(screen, (255, 255, 255), vertices)
+        print(vertices)
+        pygame.draw.polygon(screen, (255, 255, 255), vertices)
 
 
 if __name__ == '__main__':
---
+++ tiled-test.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import pygame
+import pytmx
+from pygame.locals import *
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 480, 480
+FPS = 60
+DATA_FILE = 'data'
+TILE_SIZE = 16
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join(DATA_FILE, name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def link_file(name):
+    if not isinstance(name, str):
+        raise TypeError('file name must be str type')
+    fullname = os.path.join(DATA_FILE, name)
+    if os.path.exists(fullname):
+        return fullname
+    else:
+        raise FileNotFoundError(f'Cannot find image: {name}')
+
+
+class Labyrinth:
+    def __init__(self, filename, free_tiles):
+        self.map = pytmx.load_pygame(
+            link_file(filename))  # ToDo почему то просит хранить tmx и png файлы в data/data
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_height = self.map.tileheight
+        self.tile_width = self.map.tilewidth
+        self.free_tiles = free_tiles
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                image = self.map.get_tile_image(x, y, 0)
+                if image is not None:
+                    screen.blit(image, (x * self.tile_width, y * self.tile_height))
+
+    def get_tile_id(self, pos):
+        return self.map.tiledgidmap[self.map.get_tile_gid(*pos, 0)]
+
+    def is_free(self, pos):  # ToDO как это раотает?
+        return self.get_tile_id(pos) in self.free_tiles
+
+    def find_path_step(self):  # и это тоже...
+        pass
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    labyrinth = Labyrinth('map1.tmx', [0, 30])
+    clock = pygame.time.Clock()
+    while 1:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.fill((119, 128, 225))
+        labyrinth.render(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    main()
